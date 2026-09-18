7b884c2be1a54359a36aefd21628560f9ca84e8d
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-19 00:27:19 +0530

    Consolidate performance dashboard state

--- app/src/main/java/com/rajankumar/encyclopaedia/feature/performance/PerformanceDashboardState.kt
+++
@@ -1,7 +0,0 @@
-package com.rajankumar.encyclopaedia.feature.performance
-
-data class PerformanceDashboardState(
-  val period: PerformancePeriod = PerformancePeriod.ALL,
-  val weakQuestionQuery: String = "",
-  val weakQuestionSort: PerformanceSort = PerformanceSort.MOST_MISTAKES
-)
