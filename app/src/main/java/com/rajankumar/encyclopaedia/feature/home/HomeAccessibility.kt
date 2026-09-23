99e9bea0fe4e927b493b18e451206917dfc57dfc
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-23 15:43:48 +0530

    Add home dashboard accessibility descriptions

---
+++ app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeAccessibility.kt
@@ -0,0 +1,5 @@
+package com.rajankumar.encyclopaedia.feature.home
+
+fun HomeStatModel.accessibilityDescription(): String = "$title: $value. $detail"
+
+fun HomePlanRecommendation.accessibilityDescription(): String = "$title. $detail"
