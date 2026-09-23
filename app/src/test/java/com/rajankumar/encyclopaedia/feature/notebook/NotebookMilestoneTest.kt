ea07e3245e65834cbbee077d330dd8971d30c5c0
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-23 16:28:23 +0530

    Test notebook canvas milestone

---
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/notebook/NotebookMilestoneTest.kt
@@ -0,0 +1,8 @@
+package com.rajankumar.encyclopaedia.feature.notebook
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class NotebookMilestoneTest {
+  @Test fun notebookCanvasCapabilitiesArePresent() = assertTrue(notebookCanvasMilestone.complete)
+}
