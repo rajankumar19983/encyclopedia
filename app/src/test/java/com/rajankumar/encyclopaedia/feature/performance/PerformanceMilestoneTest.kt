b9b07494ee6c7c7892178f0e6cb48b7ca4226ba3
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-23 16:51:55 +0530

    Remove obsolete duplicate milestone test

--- app/src/test/java/com/rajankumar/encyclopaedia/feature/performance/PerformanceMilestoneTest.kt
+++
@@ -1,8 +0,0 @@
-package com.rajankumar.encyclopaedia.feature.performance
-
-import org.junit.Assert.assertTrue
-import org.junit.Test
-
-class PerformanceMilestoneTest {
-  @Test fun analyticsMilestoneIsComplete() = assertTrue(currentPerformanceMilestone.complete)
-}
