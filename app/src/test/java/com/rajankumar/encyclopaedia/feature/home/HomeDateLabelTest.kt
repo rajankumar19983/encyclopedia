d13ffa499102c7577769003c56ac21b337be5888
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:34:50 +0530

    test: cover dashboard date label

---
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabelTest.kt
@@ -0,0 +1,9 @@
+package com.rajankumar.encyclopaedia.feature.home
+
+import java.time.LocalDate
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class HomeDateLabelTest {
+  @Test fun formatsReadableEnglishDate() = assertEquals("Thursday, September 24", homeDateLabel(LocalDate.of(2026, 9, 24)))
+}
