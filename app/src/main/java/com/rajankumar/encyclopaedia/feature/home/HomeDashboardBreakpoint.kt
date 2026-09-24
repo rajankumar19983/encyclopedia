53fb535ebe0450e5bfe8d07d912e96e81ead2d69
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:46:30 +0530

    feat: expose dashboard column count by width

--- app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeDashboardBreakpoint.kt
+++ app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeDashboardBreakpoint.kt
@@ -7,3 +7,9 @@ fun homeDashboardSize(widthDp: Int): HomeDashboardSize = when {
   widthDp >= 700 -> HomeDashboardSize.MEDIUM
   else -> HomeDashboardSize.COMPACT
 }
+
+fun HomeDashboardSize.statColumns(): Int = when (this) {
+  HomeDashboardSize.COMPACT -> 1
+  HomeDashboardSize.MEDIUM -> 2
+  HomeDashboardSize.EXPANDED -> 4
+}
