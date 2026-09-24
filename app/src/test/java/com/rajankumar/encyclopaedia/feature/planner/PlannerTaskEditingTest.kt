f79f20f25bb1cf258322c95478c704b6c431c124
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 18:11:07 +0530

    test: cover planner edit length validation

--- app/src/test/java/com/rajankumar/encyclopaedia/feature/planner/PlannerTaskEditingTest.kt
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/planner/PlannerTaskEditingTest.kt
@@ -6,19 +6,11 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class PlannerTaskEditingTest {
-  @Test
-  fun editValueIsLimitedToPlannerTitleLength() {
-    assertEquals(160, plannerTaskEditValue("x".repeat(200)).length)
-  }
-
-  @Test
-  fun saveRejectsBlankAndAcceptsUsefulTitle() {
+  @Test fun editValueIsLimitedToPlannerTitleLength() = assertEquals(MAX_PLANNER_TASK_LENGTH, plannerTaskEditValue("x".repeat(200)).length)
+  @Test fun saveRejectsBlankAndAcceptsUsefulTitle() {
     assertFalse(canSavePlannerTaskEdit("   "))
     assertTrue(canSavePlannerTaskEdit("Revise operating systems"))
   }
-
-  @Test
-  fun savedTitleUsesPlannerNormalization() {
-    assertEquals("Revise DBMS", savedPlannerTaskTitle("  Revise   DBMS  "))
-  }
+  @Test fun oversizedRawEditIsRejected() = assertFalse(canSavePlannerTaskEdit("x".repeat(MAX_PLANNER_TASK_LENGTH + 1)))
+  @Test fun savedTitleUsesPlannerNormalization() = assertEquals("Revise DBMS", savedPlannerTaskTitle("  Revise   DBMS  "))
 }
