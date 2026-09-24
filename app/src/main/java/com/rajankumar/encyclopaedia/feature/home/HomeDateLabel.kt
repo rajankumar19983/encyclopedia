25e9ab3b001a4b26cf6e072863b612a1c6dad110
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:34:43 +0530

    feat: add dashboard date label formatting

---
+++ app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabel.kt
@@ -0,0 +1,9 @@
+package com.rajankumar.encyclopaedia.feature.home
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+private val dashboardDateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d", Locale.ENGLISH)
+
+fun homeDateLabel(date: LocalDate = LocalDate.now()): String = date.format(dashboardDateFormatter)
