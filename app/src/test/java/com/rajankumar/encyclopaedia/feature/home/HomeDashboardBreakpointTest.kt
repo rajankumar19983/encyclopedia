5fd6e86a804cf07dce9ddf7bc97ab081bd6ec6f1
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:46:43 +0530

    test: cover responsive dashboard stat columns

--- app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeDashboardBreakpointTest.kt
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeDashboardBreakpointTest.kt
@@ -7,4 +7,9 @@ class HomeDashboardBreakpointTest {
   @Test fun compactBelowTabletWidth() = assertEquals(HomeDashboardSize.COMPACT, homeDashboardSize(699))
   @Test fun mediumOnTablet() = assertEquals(HomeDashboardSize.MEDIUM, homeDashboardSize(900))
   @Test fun expandedOnWideTablet() = assertEquals(HomeDashboardSize.EXPANDED, homeDashboardSize(1200))
+  @Test fun statColumnsFollowAvailableWidth() {
+    assertEquals(1, HomeDashboardSize.COMPACT.statColumns())
+    assertEquals(2, HomeDashboardSize.MEDIUM.statColumns())
+    assertEquals(4, HomeDashboardSize.EXPANDED.statColumns())
+  }
 }
