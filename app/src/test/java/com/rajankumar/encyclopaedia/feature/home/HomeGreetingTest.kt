1f0a1db88e3e6cb4fe44035087c8ff795e0b799d
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:45:49 +0530

    test: cover personalized dashboard greeting

--- app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeGreetingTest.kt
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeGreetingTest.kt
@@ -9,4 +9,5 @@ class HomeGreetingTest {
   @Test fun afternoonGreeting() = assertEquals("Good afternoon", homeGreeting(LocalTime.of(14, 0)))
   @Test fun eveningGreeting() = assertEquals("Good evening", homeGreeting(LocalTime.of(19, 0)))
   @Test fun lateNightGreeting() = assertEquals("Welcome back", homeGreeting(LocalTime.of(1, 0)))
+  @Test fun personalizedGreetingTrimsName() = assertEquals("Good morning, Rajan", homeGreeting(LocalTime.of(8, 0), "  Rajan "))
 }
