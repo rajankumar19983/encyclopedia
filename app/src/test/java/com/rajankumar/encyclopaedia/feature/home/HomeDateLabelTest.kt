28b001136e35e4bb73ebfbd006d2735e224fe972
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:46:15 +0530

    test: cover complete dashboard date label

--- app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabelTest.kt
+++ app/src/test/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabelTest.kt
@@ -5,5 +5,5 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class HomeDateLabelTest {
-  @Test fun formatsReadableEnglishDate() = assertEquals("Thursday, September 24", homeDateLabel(LocalDate.of(2026, 9, 24)))
+  @Test fun formatsReadableEnglishDate() = assertEquals("Thursday, September 24, 2026", homeDateLabel(LocalDate.of(2026, 9, 24)))
 }
