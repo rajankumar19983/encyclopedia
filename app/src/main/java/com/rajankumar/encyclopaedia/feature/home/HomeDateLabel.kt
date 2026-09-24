e6999d189ad322ab5d1cc6222d6a2fdafff8cfb9
Rajan Choudhary <52191478+rajankumar19983@users.noreply.github.com>
2026-09-24 17:46:03 +0530

    feat: include year in dashboard date label

--- app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabel.kt
+++ app/src/main/java/com/rajankumar/encyclopaedia/feature/home/HomeDateLabel.kt
@@ -4,6 +4,6 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-private val dashboardDateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d", Locale.ENGLISH)
+private val dashboardDateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy", Locale.ENGLISH)
 
 fun homeDateLabel(date: LocalDate = LocalDate.now()): String = date.format(dashboardDateFormatter)
